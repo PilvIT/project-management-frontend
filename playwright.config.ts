cc2a0d0f924844ed10de3baf91651ba02e776dbf
Chi-Hao Lay <chi-hao.lay@outlook.com>
2022-02-21 13:13:53 +0200

    Try testing using development mode in GitHub actions

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,12 @@ import { PlaywrightTestConfig, devices } from "@playwright/test";
 // https://github.com/microsoft/playwright/blob/master/packages/playwright-core/src/server/deviceDescriptorsSource.json
 const config: PlaywrightTestConfig = {
   globalSetup: "playwright.setup.ts",
+  webServer: {
+    command: "yarn dev",
+    port: 3000,
+    timeout: 120_000,
+    reuseExistingServer: !process.env.CI,
+  },
   projects: [
     {
       name: "chromium",
